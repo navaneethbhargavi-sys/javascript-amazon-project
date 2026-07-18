6924399325628c3fced26a704282dd9eaca88d90
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-18 11:50:08 +0530

    Added productId to every update link and console.logged it on click

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -36,7 +36,7 @@ cart.forEach((cartItem) => {
                 <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                 </span>
-                <span class="update-quantity-link link-primary">
+                <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id="${matchingProduct.id}">
                 Update
                 </span>
                 <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
@@ -107,6 +107,12 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
 
       updateCartQuantity();
+    });
+  });
+
+  document.querySelectorAll('.js-update-quantity-link').forEach((updateLink) => {
+    updateLink.addEventListener('click', () => {
+      console.log(updateLink.dataset.productId);
     });
   });
 
