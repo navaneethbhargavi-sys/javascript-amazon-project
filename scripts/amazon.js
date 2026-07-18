d11c341fd54e74318b97b94edded4856e7b38664
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-18 11:31:58 +0530

    updae cart quantity on the main page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,6 +70,8 @@ function updateCartQuantity() {
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 };
 
+updateCartQuantity();
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
