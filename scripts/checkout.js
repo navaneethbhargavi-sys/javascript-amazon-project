887f00346664d809abc0e292c1e71179a4279d17
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-18 17:21:45 +0530

    updated cart quantity on save click

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity } from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -16,84 +16,84 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-            <div class="delivery-date">
-              Delivery date: Tuesday, June 21
-            </div>
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="delivery-date">
+          Delivery date: Tuesday, June 21
+        </div>
 
-            <div class="cart-item-details-grid">
-              <img class="product-image"
-                src="${matchingProduct.image}">
+        <div class="cart-item-details-grid">
+          <img class="product-image"
+            src="${matchingProduct.image}">
 
-            <div class="cart-item-details">
-            <div class="product-name">
-                ${matchingProduct.name}
-            </div>
-            <div class="product-price">
-                $${formatCurrency(matchingProduct.priceCents)}
-            </div>
-            <div class="product-quantity">
-                <span>
-                Quantity: <span class="quantity-label js-quantity-label">${cartItem.quantity}</span>
-                </span>
-                <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id="${matchingProduct.id}">
-                Update
-                </span>
-                <input class="quantity-input js-quantity-input">
-                <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-                </span>
-            </div>
-            </div>
+        <div class="cart-item-details">
+        <div class="product-name">
+            ${matchingProduct.name}
+        </div>
+        <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+        </div>
+        <div class="product-quantity">
+            <span>
+            Quantity: <span class="quantity-label js-quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id="${matchingProduct.id}">
+            Update
+            </span>
+            <input class="quantity-input js-quantity-input">
+            <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            Delete
+            </span>
+        </div>
+        </div>
 
-            <div class="delivery-options">
-            <div class="delivery-options-title">
-                Choose a delivery option:
+        <div class="delivery-options">
+        <div class="delivery-options-title">
+            Choose a delivery option:
+        </div>
+        <div class="delivery-option">
+            <input type="radio" checked
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+            <div>
+            <div class="delivery-option-date">
+                Tuesday, June 21
             </div>
-            <div class="delivery-option">
-                <input type="radio" checked
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Tuesday, June 21
-                </div>
-                <div class="delivery-option-price">
-                    FREE Shipping
-                </div>
-                </div>
+            <div class="delivery-option-price">
+                FREE Shipping
             </div>
-            <div class="delivery-option">
-                <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Wednesday, June 15
-                </div>
-                <div class="delivery-option-price">
-                    $4.99 - Shipping
-                </div>
-                </div>
             </div>
-            <div class="delivery-option">
-                <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-                <div>
-                <div class="delivery-option-date">
-                    Monday, June 13
-                </div>
-                <div class="delivery-option-price">
-                    $9.99 - Shipping
-                </div>
-                </div>
+        </div>
+        <div class="delivery-option">
+            <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+            <div>
+            <div class="delivery-option-date">
+                Wednesday, June 15
+            </div>
+            <div class="delivery-option-price">
+                $4.99 - Shipping
+            </div>
+            </div>
+        </div>
+        <div class="delivery-option">
+            <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+            <div>
+            <div class="delivery-option-date">
+                Monday, June 13
+            </div>
+            <div class="delivery-option-price">
+                $9.99 - Shipping
             </div>
             </div>
         </div>
         </div>
-    `;
+    </div>
+    </div>
+  `;
 });
 
 document.querySelector('.js-order-summary')
@@ -120,9 +120,11 @@ document.querySelectorAll('.js-save-quantity-link').forEach((saveLink) => {
     
     container.classList.remove('is-editing-quantity');
 
-    const inputValue = document.querySelector('.js-quantity-input').value;
+    const inputValue = Number(document.querySelector('.js-quantity-input').value);
 
-    document.querySelector('.js-quantity-label').innerHTML = Number(inputValue);
+    document.querySelector('.js-quantity-label').innerHTML = inputValue;
+
+    updateQuantity(productId, inputValue);
   });
 });
 
