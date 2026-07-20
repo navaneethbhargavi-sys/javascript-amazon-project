4d4f28b678dd671a04a7361b3725b7d36dbdbe0c
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-20 11:28:08 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -130,6 +125,7 @@ export function renderOrderSummary() {
         container.remove();
 
         updateCartQuantity();
+        renderPaymentSummary();
       });
     });
 
@@ -139,6 +135,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       })
   })
 }
