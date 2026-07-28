f08b781733c02b316197961336c4a484b30a8121
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-28 15:06:02 +0530

    16l: modified updateDeliveryOption(), added test case for updating delivery optionn of a non existing product in cart

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -123,4 +123,12 @@ describe('test suite: update delivery option', () => {
       deliveryOptionId: '3'
     }]));
   });
+
+  it('update delivery option of a product not in cart', () => {
+    updateDeliveryOption('does-not-exist', '3');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+  });
 })
