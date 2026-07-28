516987782ae908e75b44d5918bc281cdde7c9968
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-28 15:00:22 +0530

    16k: test suite for update delivery option func

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage, removeFromCart } from '../../data/cart.js';
+import {addToCart, cart, loadFromStorage, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
 
 describe('test suite: add to cart', () => {
   beforeEach(() => {
@@ -94,3 +94,33 @@ describe('test suite: remove from cart', () => {
     }]));
   });
 });
+
+describe('test suite: update delivery option', () => {
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+  });
+
+  it('update delivery option of a product in cart', () => {
+    updateDeliveryOption(productId1, '3');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '3'
+    }]));
+  });
+})
