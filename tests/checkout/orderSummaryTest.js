e90b9ee948703386ce813115bba9c6f2c660e8a5
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-28 12:14:15 +0530

    actually i forgot that i didnt do 16g before now i did it lol

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -2,8 +2,8 @@ import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: render order summary', () => {
-    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
@@ -44,6 +44,12 @@ describe('test suite: render order summary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
   });
 
   it('removes a product', () => {
@@ -59,5 +65,8 @@ describe('test suite: render order summary', () => {
     ).not.toEqual(null);
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
   });
 });
