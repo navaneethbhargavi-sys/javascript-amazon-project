0b40756da1abddf2d5ea6325318f082efc6fd1db
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-30 16:47:21 +0530

    17c: added speed property and go and brake methods with speed limit between 0 and 200

--- data/car.js
+++ data/car.js
@@ -1,6 +1,7 @@
 class Car {
   brand;
   model;
+  speed = 0;
 
   constructor(carDetails) {
     this.brand = carDetails.brand;
@@ -8,21 +9,49 @@ class Car {
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+  }
+
+  go() {
+    if (this.speed < 196) {
+      this.speed += 5;
+    }
+  }
+
+  brake() {
+    if (this.speed > 4) {
+      this.speed -= 5;
+    }
   }
 }
 
-const car1 = new Car({
-  brand: 'Toyota',
-  model: 'Corolla'
-});
-const car2 = new Car({
-  brand: 'Tesla',
-  model: 'Model 3'
-});
+// const car1 = new Car({
+//   brand: 'Toyota',
+//   model: 'Corolla'
+// });
+// const car2 = new Car({
+//   brand: 'Tesla',
+//   model: 'Model 3'
+// });
 
-console.log(car1);
-console.log(car2);
+// console.log(car1);
+// console.log(car2);
 
-car1.displayInfo();
-car2.displayInfo();
+// car1.displayInfo();
+// car2.displayInfo();
+
+// car1.go();
+// car1.displayInfo();
+// car1.go();
+// car1.displayInfo();
+// car1.brake();
+// car1.displayInfo();
+// car1.brake();
+// car1.displayInfo();
+// car1.brake();
+// car1.displayInfo();
+// car1.go();
+// car1.displayInfo();
+// car1.go();
+// car1.go();
+// car1.displayInfo();
