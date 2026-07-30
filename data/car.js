124220c3715a1820f9c25157590f479b00bc57ef
Navaneeth <navaneethbhargavi@gmail.com>
2026-07-30 16:58:23 +0530

    17d: added isTrunkOpen property and openTrunk(), closeTrunk() methods with conditions for some methods and updated displayInfo()

--- data/car.js
+++ data/car.js
@@ -2,6 +2,7 @@ class Car {
   brand;
   model;
   speed = 0;
+  isTrunkOpen = false;
 
   constructor(carDetails) {
     this.brand = carDetails.brand;
@@ -9,11 +10,13 @@ class Car {
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk Status: ${
+      this.isTrunkOpen ? `open` : `closed`
+    }`);
   }
 
   go() {
-    if (this.speed < 196) {
+    if (this.speed < 196 && !this.isTrunkOpen) {
       this.speed += 5;
     }
   }
@@ -23,22 +26,32 @@ class Car {
       this.speed -= 5;
     }
   }
+
+  openTrunk() {
+    if (this.speed === 0) {
+      this.isTrunkOpen = true;
+    }
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false;
+  }
 }
 
 // const car1 = new Car({
 //   brand: 'Toyota',
 //   model: 'Corolla'
 // });
-// const car2 = new Car({
-//   brand: 'Tesla',
-//   model: 'Model 3'
-// });
+// // const car2 = new Car({
+// //   brand: 'Tesla',
+// //   model: 'Model 3'
+// // });
 
-// console.log(car1);
-// console.log(car2);
+// // console.log(car1);
+// // console.log(car2);
 
-// car1.displayInfo();
-// car2.displayInfo();
+// // car1.displayInfo();
+// // car2.displayInfo();
 
 // car1.go();
 // car1.displayInfo();
@@ -54,4 +67,10 @@ class Car {
 // car1.displayInfo();
 // car1.go();
 // car1.go();
+// car1.openTrunk();
+// car1.displayInfo();
+// car1.brake();
+// car1.brake();
+// car1.brake();
+// car1.openTrunk();
 // car1.displayInfo();
