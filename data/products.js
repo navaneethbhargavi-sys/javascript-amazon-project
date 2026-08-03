c7b5a133cb1eecbd1ce5413947329fae502d2ff7
Navaneeth <navaneethbhargavi@gmail.com>
2026-08-03 23:24:26 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -93,7 +93,9 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
-  }).then();
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
 
   return promise;
 }
@@ -120,6 +122,10 @@ export function loadProducts(fun) {
 
     fun();
   })
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
