c57662b6640f01e6d7f1b5a46e254c6ba4f03b7b
Navaneeth <navaneethbhargavi@gmail.com>
2026-08-14 10:05:23 +0530

    18o: Made the tracking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,5 +1,5 @@
-import { getOrder } from "../data/orders.js";
-import { getProduct, loadProductsFetch } from "../data/products.js";
+import { getOrder } from '../data/orders.js';
+import { getProduct, loadProductsFetch } from '../data/products.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 async function loadPage() {
@@ -14,10 +14,15 @@ async function loadPage() {
 
   let productDetails;
   order.products.forEach((details) => {
-    if (details.productId == productId) {
+    if (details.productId === product.id) {
       productDetails = details;
     };
   });
+
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
 
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
@@ -41,19 +46,25 @@ async function loadPage() {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label ${
+        percentProgress < 50 ? 'current-status' : ''
+      }">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label ${
+        (percentProgress >= 50 && percentProgress <= 100) ? 'current-status' : ''
+      }">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${
+        percentProgress >= 100 ? 'current-status' : '' 
+      }">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width= ${percentProgress}%"></div>
     </div>
   `;
 
